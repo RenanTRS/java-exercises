ca366a3c9a3d083d60d042273756b1488ca9939a
Renan Souza <renantrsouza@gmail.com>
2022-08-24 12:50:01 -0400

    add 10 class

---
+++ switches10/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        String day = "Pizza";
+
+        switch(day) {
+            case "Sunday":System.out.println("It's Sunday!");
+                break;
+            case "Monday":System.out.println("It's Monday!");
+                break;
+            case "Tuesday":System.out.println("It's Tuesday!");
+                break;
+            case "Wednesday":System.out.println("It's Wednesday!");
+                break;
+            case "Thursday":System.out.println("It's Thursday!");
+                break;
+            case "Friday":System.out.println("It's Friday!");
+                break;
+            case "Saturday":System.out.println("It's Saturday!");
+                break;
+            default:
+                System.out.println("That is not a day!");
+        }
+    }
+}
