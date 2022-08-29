44ceaf68375fae9f29216c2c1dd47cc0cb2f509d
Renan Souza <renantrsouza@gmail.com>
2022-08-29 13:58:26 -0400

    add 25 class

---
+++ finalkeyword25/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        final double pi = 3.14159; //final impede que o valor seja modificado
+
+        //pi = 4;
+
+        System.out.println(pi);
+    }
+}
