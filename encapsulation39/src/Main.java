3579e562f7b8d29380b0b6cecef4c72eed8a711a
Renan Souza <renantrsouza@gmail.com>
2022-09-01 10:37:19 -0400

    add 39 class

---
+++ encapsulation39/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Chevrolet", "Camaro", 2021);
+
+        System.out.println(car.getMake());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
+
+        //car.setModel("Onix");
+        //System.out.println(car.getModel());
+    }
+}
